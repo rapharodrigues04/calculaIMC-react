b6dc8033c7e6b73c947fff974e3f4ae9e3eb9bbe
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2022-03-04 20:30:18 -0300

    Add logica de calculo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 import image from './assets/logoImc.png';
+import { levels, calculaIMC } from './logic/imc';
 
 const App = () => {
 
---
+++ src/logic/imc.ts
@@ -0,0 +1,26 @@
+export type Level = {
+  title: string,
+  color: string,
+  icon: 'down' | 'up',
+  imc: number[],
+  yourImc?: number;
+}
+
+export const levels = [
+  { title: 'Abaixo do peso', color: '#96A3AB', icon: 'down', imc: [0, 18.5], yourImc: 0 },
+  { title: 'Peso Normal', color: '#0EAD69', icon: 'up', imc: [18.6, 24.9], yourImc: 0 },
+  { title: 'Sobrepeso', color: '#E2B039', icon: 'down', imc: [25, 29.9], yourImc: 0 },
+  { title: 'Obesidade', color: '#C3423F', icon: 'down', imc: [30, 99], yourImc: 0 },
+]
+
+export const calculaIMC = (height: number, weight: number) => {
+  const imc = weight / (height * height);
+
+  for (let i in levels) {
+    if (imc >= levels[i].imc[0] && imc < levels[i].imc[1]) {
+      levels[i].yourImc = imc;
+      return levels[i];
+    }
+  }
+  return null;
+}
